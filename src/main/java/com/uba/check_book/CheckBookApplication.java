9c5501fe2ea55a92c96a08b3c8a8aca5712d2e23
Gael Mapwata <gmapwata6@gmail.com>
2026-02-19 10:27:50 +0100

    Initial commit -Spring Boot project

---
+++ src/main/java/com/uba/check_book/CheckBookApplication.java
@@ -0,0 +1,13 @@
+package com.uba.check_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckBookApplication.class, args);
+    }
+
+}
